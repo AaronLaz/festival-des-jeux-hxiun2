e30267dcd0d5709b69df450d34bca79f796b27e1
AaronLaz <aaron.lazaroo@etu.umontpellier.fr>
2021-03-17 10:02:09 +0100

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
@@ -9,7 +10,7 @@ import { festivalDetailsComponent } from "./components/festival/festival-details
 import { MessageComponent } from "./components/shared/message/message.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   declarations: [
     AppComponent,
     HelloComponent,
--- src/app/components/festival/festival-details/festival-details.component.ts
+++ src/app/components/festival/festival-details/festival-details.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { Festival } from "../../../models/festival";
 
 @Component({
-  selector: "app-festival/festival-details",
+  selector: "app-festival-details",
   templateUrl: "festival-details.component.html",
   styleUrls: ["festival-details.component.css"]
 })
 export class festivalDetailsComponent implements OnInit {
   constructor() {}
+  @Input() festival: Festival;
+  nameControl: FormControl;
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.nameControl = new FormControl(this.festival.name);
+    this.nameControl.valueChanges.subscribe(name => {
+      this.festival.name = name;
+    });
+  }
+  valid(): void {
+    this.festival.name = this.nameControl.value;
+  }
 }
